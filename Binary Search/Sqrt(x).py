73ade9ba0ab15191d745fd5e5c2404c6bfb2c65e
Praveen Kumar Reddy <praveenkumarreddy472@gmail.com>
2026-02-10 08:16:50 +0530

    Solved rotated sorted array ||

---
+++ Binary Search/Sqrt(x).py
@@ -0,0 +1,15 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        low=0
+        high=x
+        while low<=high:
+            mid=low+(high-low)//2
+            if mid*mid==x:
+                return mid
+            elif mid*mid<x:
+                low=mid+1
+            else:
+                high=mid-1
+        return low-1
+
+        
