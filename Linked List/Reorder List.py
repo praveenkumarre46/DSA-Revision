04b74ff0c592cb71e47cb5bc5222a8b01bb15e5c
Praveen Kumar Reddy <praveenkumarreddy472@gmail.com>
2026-02-13 07:49:06 +0530

    Solved Reorder LL

---
+++ Linked List/Reorder List.py
@@ -0,0 +1,25 @@
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        if not head or not head.next:
+            return
+        
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        second = slow.next
+        slow.next = None
+        prev = None
+        while second:
+            temp = second.next
+            second.next = prev
+            prev = second
+            second = temp
+        
+        first, second = head, prev
+        while second:
+            temp1, temp2 = first.next, second.next
+            first.next = second
+            second.next = temp1
+            first, second = temp1, temp2
