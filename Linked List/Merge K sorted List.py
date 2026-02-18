98bd0125fab06e9373b643e019df9c723a497f20
Praveen Kumar Reddy <praveenkumarreddy472@gmail.com>
2026-02-18 08:23:34 +0530

    Solved Merge K sorted List

---
+++ Linked List/Merge K sorted List.py
@@ -0,0 +1,30 @@
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not lists:
+            return None
+        if len(lists) == 1:
+            return lists[0]
+
+        while len(lists) > 1:
+            merged_lists = []
+            for i in range(0, len(lists), 2):
+                l1 = lists[i]
+                l2 = lists[i + 1] if (i + 1) < len(lists) else None
+                merged_lists.append(self.mergeTwoLists(l1, l2))
+            lists = merged_lists
+
+        return lists[0]
+
+    def mergeTwoLists(self, l1, l2):
+        dummy = ListNode(0)
+        temp = dummy
+        while l1 and l2:
+            if l1.val < l2.val:
+                temp.next = l1
+                l1 = l1.next
+            else:
+                temp.next = l2
+                l2 = l2.next
+            temp = temp.next
+        temp.next = l1 or l2
+        return dummy.next
