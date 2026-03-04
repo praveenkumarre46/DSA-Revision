dd6092ef4175f0e1704c0ef4dcbd5e8f7d8d62a5
Praveen Kumar Reddy <praveenkumarreddy472@gmail.com>
2026-03-04 07:30:31 +0530

    Solved Serialise and deserialize

---
+++ Tree/Serialise and deserialize.py
@@ -0,0 +1,30 @@
+class Codec:
+    def serialize(self, root: Optional[TreeNode]) -> str:
+        res = []
+        def preorder(node):
+            if not node:
+                res.append("N")
+                return
+            res.append(str(node.val))
+            preorder(node.left)
+            preorder(node.right)
+        
+        preorder(root)
+        return ",".join(res)
+
+    def deserialize(self, data: str) -> Optional[TreeNode]:
+        vals = data.split(",")
+        self.i = 0 # Pointer to keep track of our position in the list
+
+        def dfs():
+            if vals[self.i] == "N":
+                self.i += 1
+                return None
+            
+            node = TreeNode(int(vals[self.i]))
+            self.i += 1
+            node.left = dfs()
+            node.right = dfs()
+            return node
+        
+        return dfs()
